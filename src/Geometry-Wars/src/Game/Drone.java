b2e18647203c6e627727dcdfedb6316cede4ad72
Yentis <yentl178@gmail.com>
2016-11-08 11:22:34 +0100

    stuff

--- src/Geometry-Wars/src/Game/Drone.java
+++ src/Geometry-Wars/src/Game/Drone.java
@@ -4,4 +4,34 @@ package Game;
  * Created by Yentl-PC on 8/11/2016.
  */
 public class Drone {
+    //region Instance Variables
+
+    private int dronenr;
+    private String naam;
+    private String beschrijving;
+    private int hp = 100;
+    private int kracht = 2;
+    private String uiterlijk;
+
+    //endregion
+
+    //region Constructors
+
+    public Drone(int dronenr, String naam, String beschrijving, int hp, int kracht, String uiterlijk){
+        this.dronenr = dronenr;
+        this.naam = naam;
+        this.beschrijving = beschrijving;
+        this.hp = hp;
+        this.kracht = kracht;
+        this.uiterlijk = uiterlijk;
+    }
+
+    public Drone(){
+        this.dronenr = 0;
+        this.naam = "Placeholder";
+        this.beschrijving = "Placeholder";
+        this.uiterlijk = "Vierkant";
+    }
+
+    //endregion
 }
