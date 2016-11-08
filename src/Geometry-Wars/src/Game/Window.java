5f58804ac2c89df06cb8bfc9c515bf2e9e818889
Renzie <omanarenzie@gmail.com>
2016-11-08 10:23:45 +0100

    src

---
+++ src/Geometry-Wars/src/Game/Window.java
@@ -0,0 +1,27 @@
+package Game;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Renzie on 7/11/2016.
+ */
+public class Window extends Canvas {
+
+    private static final long serialVersionUID = 573860602378245302L;
+
+    public Window(int width, int height, String title, Game game){
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width,height));
+        frame.setMaximumSize(new Dimension(width,height));
+        frame.setMinimumSize(new Dimension(width,height));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+    }
+}
