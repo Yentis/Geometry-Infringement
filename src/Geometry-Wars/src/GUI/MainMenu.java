23f9148bff26d9fac28930c58ab29c56efe1677c
Renzie <omanarenzie@gmail.com>
2016-11-08 11:57:05 +0100

    bg

--- src/Geometry-Wars/src/GUI/MainMenu.java
+++ src/Geometry-Wars/src/GUI/MainMenu.java
@@ -1,8 +1,20 @@
 package GUI;
 
+import sun.applet.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 /**
  * Created by Renzie on 8/11/2016.
  */
-public class MainMenu {
+public class MainMenu extends JFrame{
 
+    private JPanel Background;
+
+
+    private void createUIComponents() {
+        Background = new ImagePanel();
+    }
 }
