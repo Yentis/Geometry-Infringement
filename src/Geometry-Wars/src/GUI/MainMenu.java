595ee0a522c356d3a94aa6986498119ebac4a63a
Renzie <omanarenzie@gmail.com>
2016-11-08 12:15:47 +0100

    bg werkt PogChamp

--- src/Geometry-Wars/src/GUI/MainMenu.java
+++ src/Geometry-Wars/src/GUI/MainMenu.java
@@ -5,16 +5,22 @@ import sun.applet.Main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 
 /**
  * Created by Renzie on 8/11/2016.
  */
 public class MainMenu extends JFrame{
 
-    private JPanel Background;
 
+    private JButton bg;
+
+    public MainMenu() {
+
+    }
 
     private void createUIComponents() {
-        Background = new ImagePanel();
+        bg.add(new JLabel(new ImageIcon("../Media/Background.png")));
+
     }
 }
